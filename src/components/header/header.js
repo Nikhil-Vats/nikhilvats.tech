85572abfbce51dd2f410069ef9ba3ee2a806ff02
Nikhil-Vats <nikvats5499@gmail.com>
2020-01-30 04:34:37 +0530

    Added menu icon for phones

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,7 +6,8 @@ import headerStyles from './header.module.scss';
 import github from "../../assets/github.svg";
 import linkedin from "../../assets/linkedin.svg";
 import twitter from "../../assets/twitter.svg";
-import logo from "../../assets/logo.svg";
+import email from "../../assets/email.svg";
+import menu from "../../assets/menu.svg";
 
 const Header = () => {
     const data = useStaticQuery(graphql`
@@ -18,18 +19,25 @@ const Header = () => {
             }
         }
     `)
+    let dispState = false;
+    let icons = React.createRef();
+    function showMenu() {
+        icons.current.style.transform = dispState ? 'rotateX(90deg)' : 'rotateX(0deg)';
+        dispState = !dispState;
+    }
     return (
         <header className={[headerStyles.header,headerStyles.safari_only].join(" ")}>
-            {/* <h1><Link className={headerStyles.title} to="/">{data.site.siteMetadata.title}</Link></h1> */}
+            {/* <h1 onClick={() => showMenu()}>{data.site.siteMetadata.title}</h1> */}
             <nav>
                 <ul className={headerStyles.navList}>
-                    <li><img src={logo} /></li>
                     <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
                     <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
                     <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link></li>
                     <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link></li>
                 </ul>
-                <ul className={headerStyles.socialLinks}>
+                <span><img onClick={() => showMenu()} src={menu} className={headerStyles.menu}/></span>
+                <ul ref={icons} className={headerStyles.socialLinks}>
+                    <li><a href="mailto:nikvats5499@gmail.com" target="_blank"><img src={email} /></a></li>
                     <li><a href="https://github.com/Nikhil-Vats" target="_blank"><img src={github} /></a></li>
                     <li><a href="https://www.linkedin.com/in/nikhil-vats/" target="_blank"><img src={linkedin} /></a></li>
                     <li><a href="https://twitter.com/NikhilVatss" target="_blank"><img src={twitter} /></a></li>
