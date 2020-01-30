1cb925d13597c30f048d8f3007521bf101990032
Nikhil-Vats <nikvats5499@gmail.com>
2020-01-30 14:58:47 +0530

    Completed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,17 @@ import React from "react";
 import Layout from "../components/layout/layout";
 import { Link } from "gatsby";
 import Head from "../components/head";
+import underConstruction from "../assets/under_construction.svg";
+import FourOFourStyles from "../styles/fourOFour.module.scss";
 const NotFound = () => {
     return (
         <Layout>
             <Head title="404" />
-            <p>Page not found</p>
-            <p><Link to="/">Head Home</Link></p>
+            <div className={FourOFourStyles.layout}>
+                <img className={FourOFourStyles.underConstruction} src={underConstruction} />
+                <h3>Uh Oh! Page not found!</h3>
+                <h3><Link className={FourOFourStyles.link} to="/">Head Home?</Link></h3>
+            </div>
         </Layout>
     )
 }
